7ae0ef3cb0ad376e662641141c35267bb982858a
QuyenNN <quyenguyengoc911@gmail.com>
2020-02-17 19:07:40 +0900

    'static-html'

--- src/server.js
+++ src/server.js
@@ -1,12 +1,20 @@
 import express from 'express';
 
 import connectDB from './config/connectDB';
+import configViewEngine from './config/viewEngine';
 
 let app = express();
+
 connectDB();
 
-app.get("/helloworld", (req, res) => {
-  res.send(`<h1>Hello world!!!</h1>`)
+configViewEngine(app);
+
+app.get("/", (req, res) => {
+  return res.render('main/home');
+});
+
+app.get("/login", (req, res) => {
+  return res.render('session/login');
 });
 
 app.listen(process.env.APP_PORT, process.env.APP_HOST, () => {
